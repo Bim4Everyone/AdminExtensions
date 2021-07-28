05567f82959b1b5435fd39c591608d50da004cf3
dosymep <dosymep@gmail.com>
2021-07-28 14:13:51 +0300

    Изменил работу плагина параметров

--- Admin.tab/DevTools.panel/Parameters.pushbutton/script.py
+++ Admin.tab/DevTools.panel/Parameters.pushbutton/script.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from System.IO import File
-from System.Text import Encoding
-from System.Diagnostics import Process
+from System.Collections.Generic import List
 
 from Autodesk.Revit.DB import *
-
-from pyrevit import forms
 
 document = __revit__.ActiveUIDocument.Document
 
 elements = FilteredElementCollector(document).OfClass(SharedParameterElement).WhereElementIsNotElementType().ToElements()
 if elements:
-    result = [ "GUID;Id;ParamName" ]
-    for element in elements:
-        result.append(str(element.GuidValue) +";" + str(element.Id.IntegerValue) + ";" + element.Name)
+    elements = [ element for element in elements if element.GetDependentElements(None).Count == 1  ]
+    print "\r\n".join([ str(element.GuidValue) +" - " + str(element.Id.IntegerValue) + " - " + element.Name for element in elements ])
 
-    fileName = forms.save_file(files_filter="csv files (*.csv)|*.csv", default_name="Параметры")
-    if fileName:
-        File.WriteAllText(fileName, "\r\n".join(result), Encoding.GetEncoding(1251))
-        Process.Start(fileName)
+    with Transaction(document) as transaction:
+        transaction.Start("Удаление параметров")
+
+        document.Delete(List[ElementId]([ element.Id for element in elements ]))
+
+        transaction.Commit()
