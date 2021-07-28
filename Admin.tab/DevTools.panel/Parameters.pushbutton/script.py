e661e23a0e6cbb07f059ad7ac0623467faf3a3c6
dosymep <dosymep@gmail.com>
2021-07-28 15:13:36 +0300

    Изменил работу плагина параметров

--- Admin.tab/DevTools.panel/Parameters.pushbutton/script.py
+++ Admin.tab/DevTools.panel/Parameters.pushbutton/script.py
@@ -9,11 +9,13 @@ document = __revit__.ActiveUIDocument.Document
 elements = FilteredElementCollector(document).OfClass(SharedParameterElement).WhereElementIsNotElementType().ToElements()
 if elements:
     elements = [ element for element in elements if element.GetDependentElements(None).Count == 1  ]
-    print "\r\n".join([ str(element.GuidValue) +" - " + str(element.Id.IntegerValue) + " - " + element.Name for element in elements ])
-
     with Transaction(document) as transaction:
         transaction.Start("Удаление параметров")
-
-        document.Delete(List[ElementId]([ element.Id for element in elements ]))
+        for element in elements:
+            try:
+                print str(element.GuidValue) +" - " + str(element.Id.IntegerValue) + " - " + element.Name
+                document.Delete(element.Id)
+            except Exception as ex:
+                print str(ex)
 
         transaction.Commit()
