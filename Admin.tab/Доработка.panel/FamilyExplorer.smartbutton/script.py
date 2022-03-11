34f9f1d35d578a2c6763f07a58403e1c88660d1a
dosymep <dosymep@gmail.com>
2022-03-11 17:24:08 +0300

    Обновил библиотеки расширений Revit

--- Admin.tab/Доработка.panel/FamilyExplorer.smartbutton/script.py
+++ Admin.tab/Доработка.panel/FamilyExplorer.smartbutton/script.py
@@ -17,11 +17,11 @@ import RevitFamilyExplorer
 def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
     # чтобы нормально работала панель,
     # она должна быть инициализированная заранее
-    RevitFamilyExplorer.RegisterFamilyExplorerCommand().Execute(__rvt__)
+    RevitFamilyExplorer.RegisterFamilyExplorerCommand().RegisterPanel(__rvt__)
 
 
 def open_family_explorer():
-    RevitFamilyExplorer.FamilyExplorerCommand().Execute(__revit__)
+    RevitFamilyExplorer.FamilyExplorerCommand().ChangeVisiblePanel(__revit__)
 
 
 if __name__ == '__main__':
