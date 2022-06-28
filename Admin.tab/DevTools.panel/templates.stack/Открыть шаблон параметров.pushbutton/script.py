ab3fc0bfe029b10e6a7eb07b71ab32171af1e727
dosymep <dosymep@gmail.com>
2022-06-28 11:35:08 +0300

    Добавил скрипт открытия файла шаблона

---
+++ Admin.tab/DevTools.panel/templates.stack/Открыть шаблон параметров.pushbutton/script.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import clr
+clr.AddReference("dosymep.Revit.dll")
+clr.AddReference("dosymep.Bim4Everyone.dll")
+
+import dosymep
+clr.ImportExtensions(dosymep.Revit)
+clr.ImportExtensions(dosymep.Bim4Everyone)
+
+from pyrevit import forms
+from pyrevit import EXEC_PARAMS
+
+from dosymep.Revit import *
+from dosymep.Bim4Everyone import *
+from dosymep_libs.bim4everyone import *
+
+from Autodesk.Revit.DB import *
+
+
+@notification()
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    __revit__.OpenAndActivateDocument(ModuleEnvironment.ParametersTemplatePath)
+
+
+script_execute()
