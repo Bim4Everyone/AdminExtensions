6dd6b944885999a0cee90e8022c7e1c1fb4a45b9
dosymep <dosymep@gmail.com>
2022-11-08 16:00:45 +0300

    Обновил путь до пустого файла

--- Admin.tab/Стандартизация.panel/CreateProject.pushbutton/script.py
+++ Admin.tab/Стандартизация.panel/CreateProject.pushbutton/script.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import clr
 clr.AddReference("System.Windows.Forms")
 
 from System.Windows.Forms import *
+
 from Autodesk.Revit.DB import *
+from Autodesk.Revit.ApplicationServices import *
 
-templateFilePath = "T:\\Проектный институт\\Отдел стандартизации BIM и RD\\BIM-Ресурсы\\5-Надстройки\\Шаблоны и настройки\\Test\\Пустой.rte"
+from pyrevit import EXEC_PARAMS
 
-application = __revit__.Application
+from dosymep_libs.bim4everyone import log_plugin
+from dosymep_libs.simple_services import notification
 
-def GetFilePath():
+application = __revit__.Application # type: Application
+
+template_file_path = r"T:\Проектный институт\Отдел стандартизации BIM и RD\BIM-Ресурсы\5-Надстройки\Bim4Everyone\A101"
+template_file_path = os.path.join(template_file_path, application.VersionNumber, "RevitCopyStandarts")
+
+def get_file_path():
     with SaveFileDialog() as dialog:
         dialog.RestoreDirectory = True
         dialog.Filter = "Revit files (*.rvt)|*.rvt"
@@ -20,11 +30,16 @@ def GetFilePath():
 
     return None
 
+@notification()
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    file_name = get_file_path()
+    if file_name:
+        document = application.NewProjectDocument(template_file_path)
+        document.SaveAs(file_name, SaveAsOptions())
+        document.Close(False)
 
-fileName = GetFilePath()
-if(fileName):
-    document = application.NewProjectDocument(templateFilePath)
-    document.SaveAs(fileName, SaveAsOptions(OverwriteExistingFile=True))
-    document.Close(False)
+        __revit__.OpenAndActivateDocument(file_name)
 
-    __revit__.OpenAndActivateDocument(fileName)
+
+script_execute()
